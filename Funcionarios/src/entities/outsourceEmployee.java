e481959cd162ff3fafc206f9714e43ad63974973
Felipe <felipemiguelfutebol@hotmail.com>
2024-05-10 17:13:46 -0300

    Projeto finalizado.

---
+++ Funcionarios/src/entities/outsourceEmployee.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class outsourceEmployee extends Employee {
+
+	private double additionalCharge;
+
+	public outsourceEmployee() {
+		super();
+	}
+
+	public outsourceEmployee(String name, Integer hours, double valuePerHour, double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+
+}
